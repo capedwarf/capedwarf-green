d9b6059dd063dbcddb434cea3de0eeaea0e6f3d6
Ales Justin <ales.justin@gmail.com>
2011-04-15 15:59:57 +0200

    Add BM lookup.

---
+++ server-jee/src/main/java/org/jboss/lhotse/server/jee/servlet/JeeBeanManagerLookup.java
@@ -0,0 +1,79 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.lhotse.server.jee.servlet;
+
+import java.util.logging.Logger;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+
+import org.jboss.lhotse.server.api.servlet.BeanManagerLookup;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * JEE BM lookup.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@MetaInfServices
+public class JeeBeanManagerLookup implements BeanManagerLookup
+{
+   private static Logger log = Logger.getLogger(JeeBeanManagerLookup.class.getName());
+   private static final String STANDARD_BEAN_MANAGER_JNDI_NAME = "java:comp/BeanManager";
+
+   private BeanManager beanManager;
+
+   public BeanManager lookup(ServletContext context)
+   {
+      Context nc = null;
+      try
+      {
+         if (beanManager == null)
+         {
+            nc = new InitialContext();
+            beanManager = (BeanManager) nc.lookup(STANDARD_BEAN_MANAGER_JNDI_NAME);
+         }
+         return beanManager;
+      }
+      catch (Exception e)
+      {
+         log.warning("Cannot find BeanManager: " + e);
+         return null;
+      }
+      finally
+      {
+         if (nc != null)
+         {
+            try
+            {
+               nc.close();
+            }
+            catch (NamingException ignored)
+            {
+            }
+         }
+      }
+   }
+}
